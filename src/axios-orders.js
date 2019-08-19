052d0eefffd161565479132e7a40404535e6f17c
Tony <iamtonycheung@gmail.com>
2019-08-19 18:10:14 +0800

    after lesson 252

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://tony-my-burger.firebaseio.com/'
+    baseURL: 'https://tony-react-my-burger.firebaseio.com/'
 });
 
 export default instance;
